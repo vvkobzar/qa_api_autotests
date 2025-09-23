07e99806ea3e60d7a5726919bfa3caa5928f62f3
Vladislav Kobzar <kobzarvllv@gmail.com>
2025-09-23 09:19:23 +0300

    chaged uploading file in autotest

--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -1,5 +1,6 @@
 import pytest
 import allure
+from config import settings
 from fixtures.files import FileFixture
 from clients.files.files_client import FilesClient
 from tools.allure.epics import AllureEpic
@@ -30,7 +31,7 @@ class TestFiles:
     @allure.title("Create file")
     @allure.severity(Severity.BLOCKER)
     def test_create_file(self, files_client: FilesClient):
-        request = CreateFileRequestSchema(upload_file="/home/user/Pictures/ava.jpg")
+        request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = CreateFileResponseSchema.model_validate_json(response.text)
 
@@ -61,7 +62,7 @@ class TestFiles:
     def test_create_file_with_empty_filename(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
             filename="",
-            upload_file="./testdata/files/image.png"
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
@@ -79,7 +80,7 @@ class TestFiles:
     def test_create_file_with_empty_directory(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
             directory="",
-            upload_file="./testdata/files/image.png"
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
