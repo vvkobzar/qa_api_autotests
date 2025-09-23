2165d967b6037d6fea05c413bccef62bccb40695
Vladislav Kobzar <kobzarvllv@gmail.com>
2025-09-23 14:20:54 +0300

    add allure os and python version environments

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,8 +1,12 @@
+import platform
+import sys
 from config import settings
 
 
 def create_allure_environment_file():
     items = [f"{key}={value}" for key, value in settings.model_dump().items()]
+    items.append(f"OS={platform.system()}, {platform.release()}")
+    items.append(f"Python.Version={sys.version}")
     properties = "\n".join(items)
 
     with open(settings.allure_results_dir.joinpath('environment.properties'), "w+") as file:
